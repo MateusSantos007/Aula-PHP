60a1101bd921b219fd5f39275e559fd6b9f59961
MateusSantos007 <163445278+MateusSantos007@users.noreply.github.com>
2024-10-08 21:52:33 -0300

    Add files via upload

---
+++ livro2/recebeid.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="apaga.php">
+        <label for="id">ID:</label>
+        <Input type="text" name="id"></Input>
+        <input type="submit" value="Apagar"></input>
+    </form>
+    
+</body>
+</html>
